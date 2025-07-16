4f2830fad94eba9ce5e2ac543d284ed911d65ce9
Arya Jagadditha <czajr13@gmail.com>
2025-07-16 20:17:44 +0700

    feat: add services section

--- src/components/AnimatedTextLines.jsx
+++ src/components/AnimatedTextLines.jsx
@@ -1,15 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import { useRef } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
-
-const AnimatedTextLine = ({ text, className }) => {
+export const AnimatedTextLines = ({ text, className }) => {
   const containerRef = useRef(null);
   const lineRefs = useRef([]);
-  const lines = text.split("\n").filter((line) => line.trim() !== "")
-
+  const lines = text.split("\n").filter((line) => line.trim() !== "");
   useGSAP(() => {
     if (lineRefs.current.length > 0) {
       gsap.from(lineRefs.current, {
@@ -20,23 +17,22 @@ const AnimatedTextLine = ({ text, className }) => {
         ease: "back.out",
         scrollTrigger: {
           trigger: containerRef.current,
-        }
-      })
+        },
+      });
     }
-  })
-  
+  });
+
   return (
     <div ref={containerRef} className={className}>
       {lines.map((line, index) => (
-        <span 
-          key={index} 
+        <span
+          key={index}
           ref={(el) => (lineRefs.current[index] = el)}
-          className="block leading-relaxed tracking-wide text-pretty">
+          className="block leading-relaxed tracking-wide text-pretty"
+        >
           {line}
         </span>
       ))}
     </div>
-  )
-}
-
-export default AnimatedTextLine
+  );
+};
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,59 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import { Planet } from "../components/Planet";
 import { Environment, Float, Lightformer } from "@react-three/drei";
-import React, { useRef } from 'react'
-import AnimatedTextLine from '../components/AnimatedTextLine'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
 import { useMediaQuery } from "react-responsive";
-
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
 const Hero = () => {
-  const contextRef = useRef(null)
-  const headerRef = useRef(null)
-  const aboutText = `I help growing brands and startups gain an
-  unfair advantage through premium
-  results driven webs/apps`;
-
   const isMobile = useMediaQuery({ maxWidth: 853 });
-
-  useGSAP(() => {
-    const tl = gsap.timeline();
-
-    tl.from(contextRef.current, {
-      y: "50vh",
-      duration: 1,
-      ease: "circ.out",
-    })
-    tl.from(headerRef.current, {
-      opacity: 0, 
-      y: "200",
-      duration: 1,
-      ease: 'circ.out'
-    }, "<+0.2")
-  }, [])
-
+  const text = `I help growing brands and startups gain an
+unfair advantage through premium
+results driven webs/apps`;
   return (
-    <section id='home' className='flex flex-col justify-end min-h-screen'>
-      <div ref={contextRef}>
-        <div style={{ clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)" }}>
-          <div ref={headerRef} className='flex flex-col justify-center gap-12 pt-16 sm:gap-16'>
-            <p className='text-sm font-light tracking-[0.5rem] uppercase px-10 text-black'>
-              404 No Bugs Found
-            </p>
-            <div>
-              <h1 className='flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive mb-4 sm:gap-16 md:block'>XTRAHERA</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="relative px-10 text-black">
-        <div className='absolute inset-x-0 border-t-2' />
-        <div className='py-12 sm:py-16 text-end'>
-          <AnimatedTextLine 
-            text={aboutText}
-            className='font-light uppercase value-text-responsive' />
-        </div>
-      </div>
+    <section id="home" className="flex flex-col justify-end min-h-screen">
+      <AnimatedHeaderSection
+        subTitle={"404 No Bugs Found"}
+        title={"XTRAHERA"}
+        text={text}
+        textColor={"text-black"}
+      />
       <figure
         className="absolute inset-0 -z-50"
         style={{ width: "100vw", height: "100vh" }}
@@ -97,7 +59,7 @@ const Hero = () => {
         </Canvas>
       </figure>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
