6696468e680e712b027e48daac3d4996d210fde9
Arya Jagadditha <czajr13@gmail.com>
2025-07-16 19:43:46 +0700

    feat: finish implement hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,31 +1,101 @@
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "../components/Planet";
+import { Environment, Float, Lightformer } from "@react-three/drei";
 import React, { useRef } from 'react'
+import AnimatedTextLine from '../components/AnimatedTextLine'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
   const contextRef = useRef(null)
   const headerRef = useRef(null)
+  const aboutText = `I help growing brands and startups gain an
+  unfair advantage through premium
+  results driven webs/apps`;
+
+  const isMobile = useMediaQuery({ maxWidth: 853 });
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.from(contextRef.current, {
+      y: "50vh",
+      duration: 1,
+      ease: "circ.out",
+    })
+    tl.from(headerRef.current, {
+      opacity: 0, 
+      y: "300",
+      duration: 1,
+      ease: 'circ.out'
+    }, "<+0.2")
+  }, [])
 
   return (
     <section id='home' className='flex flex-col justify-end min-h-screen'>
       <div ref={contextRef}>
-        <div style={{ clipPath: "polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)" }}>
+        <div style={{ clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)" }}>
           <div ref={headerRef} className='flex flex-col justify-center gap-12 pt-16 sm:gap-16'>
             <p className='text-sm font-light tracking-[0.5rem] uppercase px-10 text-black'>
               404 No Bugs Found
             </p>
             <div>
-              <h1 className='flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive sm:gap-16 md:block'>XTRAHERA Innovations</h1>
-            </div>
-            <div className="relative px-10 text-black">
-              <div className='absolute inset-x-0 border-t-2' />
-              <div className='py-12 sm:py-16 text-end'>
-                <p className='font-light uppercase value-text-responsive'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum corrupti soluta expedita facilis deserunt commodi iste quos dignissimos. Reiciendis illum inventore vitae voluptatem. Soluta cupiditate quibusdam saepe dolore doloribus!
-                </p>
-              </div>
+              <h1 className='flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive mb-4 sm:gap-16 md:block'>XTRAHERA</h1>
             </div>
           </div>
         </div>
       </div>
+      <div className="relative px-10 text-black">
+        <div className='absolute inset-x-0 border-t-2' />
+        <div className='py-12 sm:py-16 text-end'>
+          <AnimatedTextLine 
+            text={aboutText}
+            className='font-light uppercase value-text-responsive' />
+        </div>
+      </div>
+      <figure
+        className="absolute inset-0 -z-50"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas
+          shadows
+          camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
+        >
+          <ambientLight intensity={0.5} />
+          <Float speed={0.5}>
+            <Planet scale={isMobile ? 0.7 : 1} />
+          </Float>
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 5, -9]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 3, 1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[-5, -1, -1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[10, 1, 0]}
+                scale={16}
+              />
+            </group>
+          </Environment>
+        </Canvas>
+      </figure>
     </section>
   )
 }
